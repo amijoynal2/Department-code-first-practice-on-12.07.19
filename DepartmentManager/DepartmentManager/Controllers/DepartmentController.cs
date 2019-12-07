7d1314b7f6e8cdda44a62ed5d8e02df701faee4f
gulfam7 <shaju9218@gmail.com>
2019-12-07 12:19:48 +0600

    Department project demo 12/07/2019

---
+++ DepartmentManager/DepartmentManager/Controllers/DepartmentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentManager.Models;
+using Vereyon.Web;
+
+namespace DepartmentManager.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private ProjectDbContext projectDbContext;
+
+        public DepartmentController()
+        {
+            projectDbContext = new ProjectDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            projectDbContext.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                projectDbContext.Departments.Add(department);
+                projectDbContext.SaveChanges();
+                FlashMessage.Confirmation("Department saved successfully");
+                return RedirectToAction("Create");
+            }
+            return View();
+        }
+
+        public JsonResult IsCodeExist(string departmentcode)
+        {
+            var dpt = projectDbContext.Departments.ToList();
+            if (dpt.Any(d=>d.DepartmentCode.ToLower() == departmentcode.ToLower()))
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(true, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ DepartmentManager/DepartmentManager/Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DepartmentManager.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage = "Please enter department code")]
+        [StringLength(7,MinimumLength = 2,ErrorMessage = "Department code should be 7-2 character long")]
+        [Column(TypeName = "varchar")]
+        [Remote("IsCodeExist","Department",ErrorMessage = "Code already Exist")]
+        [DisplayName("Department Code")]
+        public string DepartmentCode { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50,ErrorMessage = "Department name should be 50 character long")]
+        [DisplayName("Department Name")]
+
+        public string DepartmentName { get; set; }
+    }
+}
